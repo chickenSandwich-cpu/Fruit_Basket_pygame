dc98748f8993063c565f14edc34dd4fe9158edc6
SaltedFries <tranblueneverdie@gmail.com>
2025-05-08 14:55:55 -0700

    added timer

--- Fruit_Basket/Fruit_Basket.py
+++ Fruit_Basket/Fruit_Basket.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import time
+pygame.font.init()
 
 # Initialize Pygame
 pygame.init()
@@ -14,11 +15,15 @@ pygame.display.set_caption("Fruit Basket")
 icon = pygame.image.load("assets/tomato.png")
 pygame.display.set_icon(icon)
 
+# Basket properties
 BASKET_WIDTH = 80
 BASKET_HEIGHT = 80
 
 BASKET_VEL = 5
 
+FONT = pygame.font.SysFont("comicsans", 30)
+
+# Load images
 BG = pygame.image.load("assets/background.png")
 basket_img = pygame.image.load("assets/basket.png")
 basket_img = pygame.transform.scale(basket_img, (BASKET_WIDTH, BASKET_HEIGHT))
@@ -28,12 +33,15 @@ basket_img = pygame.transform.scale(basket_img, (BASKET_WIDTH, BASKET_HEIGHT))
     # print("Background loaded successfully") # Debug message
 
 
-def draw(basket_img, basket_rect):
+def draw(basket_img, basket_rect, elapsed_time):
     # Draw the background image
     WIN.blit(BG, (0, 0))
 
+    # Draw the timer
+    time_text = FONT.render(f"Time: {round(elapsed_time, 1)}s", 1, "white")
+    WIN.blit(time_text, (10, 10))
+
     # Draw the basket
-    # pygame.draw.rect(WIN, (215, 139, 98), basket)
     WIN.blit(basket_img, (basket_rect.x, basket_rect.y))
 
     # Update the display
@@ -46,9 +54,13 @@ def main():
 
     basket_rect = pygame.Rect(200, HEIGHT - BASKET_HEIGHT*2, BASKET_WIDTH, BASKET_HEIGHT)
     clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
+
 
     while run:
         clock.tick(60)  # Set the frame rate to 60 FPS
+        elapsed_time = time.time() - start_time
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -61,7 +73,7 @@ def main():
         if keys[pygame.K_RIGHT] and basket_rect.x + BASKET_VEL + basket_rect.width <= WIDTH:
             basket_rect.x += BASKET_VEL
 
-        draw(basket_img, basket_rect)
+        draw(basket_img, basket_rect, elapsed_time)
 
     pygame.quit()
 
